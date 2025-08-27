0d3dd7b14d345340b79e508d2cec6ad5d30bcfa1
Nithesh <nitheshu944@gmail.com>
2025-08-27 16:36:47 +0530

    Adding Responsiveness

--- client/src/components/AppDownload.jsx
+++ client/src/components/AppDownload.jsx
@@ -3,10 +3,10 @@ import { assets } from "../assets/assets";
 
 const AppDownload = () => {
   return (
-    <div className="container px-4 sm:px-8 md:px-16 2xl:px-20  mx-auto my-20">
-      <div className="relative bg-gradient-to-r from-violet-200 to-purple-200 p:12 sm:p-24 lg:p-32 rounded-2xl">
+    <div className="container px-4 2xl:px-20  mx-auto my-20">
+      <div className="relative bg-gradient-to-r from-violet-200 to-purple-200 p-12 sm:p-24 lg:p-32 rounded-lg">
         <div>
-          <h1 className="text-2xl sm:text:4xl font-bold mb-8 max-w-md">
+          <h1 className="text-2xl sm:text-4xl font-bold mb-8 max-w-md">
             Download Mobile App for Better Experience
           </h1>
           <div className="flex gap-4">
@@ -18,7 +18,7 @@ const AppDownload = () => {
             </a>
           </div>
         </div>
-        <img className="absolute w-80 right-0 bottom-0 mr-32 mx-lg:hidden" src={assets.app_main_img} alt="" />
+        <img className="absolute w-80 right-0 bottom-0 mr-32 max-lg:hidden" src={assets.app_main_img} alt="" />
       </div>
     </div>
   );
